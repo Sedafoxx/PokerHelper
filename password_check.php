44f14712b2c701e624ff8abec55abfd57ff2afea
Sedafoxx <buryakdimitri@gmail.com>
2024-09-22 00:46:20 +0200

    modified:   password_check.php

--- password_check.php
+++ password_check.php
@@ -2,7 +2,7 @@
 session_start();
 
 if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['password'])) {
-    if ($_POST['password'] === 'IAMALLIN') {
+    if ($_POST['password'] === '') {
         // Set a session or token indicating the user has access to the download
         $_SESSION['download_allowed'] = true;
         header('Location: download_page.php'); // Redirect to the download page
