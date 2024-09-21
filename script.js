24f7cbc8b3c73e37cf8f7729ad2081374d476871
Sedafoxx <buryakdimitri@gmail.com>
2024-09-22 01:57:15 +0200

    modified:   index.html 	modified:   script.js

--- script.js
+++ script.js
@@ -243,7 +243,9 @@ document.getElementById('check-password').addEventListener('click', (event) => {
 
     // Hard-coded password check
     if (password === 'IAMALLIN') {
-        document.getElementById('apk-link').style.display = 'block'; // Show download link
+        const apkLink = document.getElementById('apk-link');
+        apkLink.style.display = 'block'; // Show download link
+        apkLink.href = 'https://edef7.pcloud.com/cBZ7nD5WMZBtQLboZZZ3iWqkkZ2ZZBU0ZkZrodk5ZbzZ97ZdHZhkZq0ZtpZA0ZDFZdJZzVZ7zZNHZFpZ9pZ3J3PZbH2mCLrt8oRwwshaqA1Maz9qWcAV/pokerhelper.apk'; // Set link dynamically
         
         // Show money rain animation
         document.getElementById('money-rain').style.display = 'block';
